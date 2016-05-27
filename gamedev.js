ad31806b3fc732e368809e0a1cf8ce94c67d9f7b
MelchiorDelloye <210436@hkis.edu.hk>
2016-05-27 09:28:21 +0800

    i added some game coding

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,28 @@
 $( document ).ready(function() {
 
-//Your jquery goes here
+$(document).keydown(function(e) {
+    switch (e.which) {
+    case 37:
+        $("#follower").stop().animate({
+            left: '-=75'
+        }); //left arrow key
+        break;
+    case 38:
+        $("#follower").stop().animate({
+            top: '-=75'
+        }); //up arrow key
+        break;
+    case 39:
+        $("#follower").stop().animate({
+            left: '+=75'
+        }); //right arrow key
+        break;
+    case 40:
+        $("#follower").stop().animate({
+            top: '+=75'
+        }); //bottom arrow key
+        break;
+    }
+})
 
 });
