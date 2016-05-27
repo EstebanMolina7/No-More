ed735539cdb3a1ffa48f3c1e048e0794040781a4
MelchiorDelloye <210436@hkis.edu.hk>
2016-05-27 10:07:30 +0800

    added

--- gamedev.js
+++ gamedev.js
@@ -3,22 +3,22 @@ $( document ).ready(function() {
 $(document).keydown(function(e) {
     switch (e.which) {
     case 37:
-        $("#follower").stop().animate({
+        $("#man").stop().animate({
             left: '-=75'
         }); //left arrow key
         break;
     case 38:
-        $("#follower").stop().animate({
+        $("#man").stop().animate({
             top: '-=75'
         }); //up arrow key
         break;
     case 39:
-        $("#follower").stop().animate({
+        $("#man").stop().animate({
             left: '+=75'
         }); //right arrow key
         break;
     case 40:
-        $("#follower").stop().animate({
+        $("#man").stop().animate({
             top: '+=75'
         }); //bottom arrow key
         break;
